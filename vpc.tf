dbcba7ac29d5197a4566efa84de4e90345668424
RyanNguyen1808 <55352985+RyanNguyen1808@users.noreply.github.com>
2025-11-06 03:02:17 +0000

    Feature/lambda (#2)
    * add lambda zip for deploy
    
    * change CI and others
    
    * obmit deloyment, trying synk only
    
    * change CI to run manually
    
    * change package scan to run manually
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * edit ci not allow run on PR
    
    * edit CI
    
    * fix CI
    
    * fix provider
    
    * fix provider
    
    * fix terraform files
    
    * fix CI
    
    * final pipelines
    
    * destroy
    
    * destrioy
    
    * falsify vpn in vpc

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.5.0"
 
   name = var.vpc_name
   cidr = "10.0.0.0/16"
@@ -8,13 +9,13 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_vpn_gateway = true
+  enable_vpn_gateway = false
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
     Created_by  = var.created_by
-    Cohort      = "CE9"
+    Cohort      = "CE11"
   }
 }
 
