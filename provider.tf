08375fc8890c0de157fde5c3b7d5c2beccbd32e9
Ryan Nguyen <nguyen@kemilinks.net>
2025-11-06 02:11:25 +0000

    fix terraform files

--- provider.tf
+++ provider.tf
@@ -1,10 +1,19 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
+
+
 }
 
 # Configure the AWS Provider
